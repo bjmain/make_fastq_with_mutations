b2e98be252a06ea9c519b5c787c3e9b435b0044d
Bradley J. Main <bjmain@users.noreply.github.com>
2020-10-05 07:32:11 -0700

    Update make_fastq_with_mutations.py

--- make_fastq_with_mutations.py
+++ make_fastq_with_mutations.py
@@ -4,9 +4,6 @@ import sys
 # python make_fastq.py oligos-ref_host_virus_acc_gene_AA_dups_v3.fasta 
 
 
-read_len = 100
-step_size = 100
-
 D={}
 for line in open(sys.argv[1]):
         i=line.strip().split()
@@ -22,9 +19,6 @@ for pep in D:
     seq = D[pep]
     for base in range(len(seq)):
         bases = ["A", "T", "C", "G"]
-        #print(seq)
-        #print(base)
-        #print(seq[base])
         bases.remove(seq[base])  
         seq = list(seq)
         fake_mutation = bases[0] 
@@ -36,13 +30,5 @@ for pep in D:
         print("".join(seq))
         
         print("+")
-        fake_quality = "J" * int(len(seq))
-        print(fake_quality)   # This is simplistic
-        #break
-    #seq_list = [seq[i:i+read_len] for i in xrange(0, len(seq), step_size)] # len(seq) allows for the last read to be less than 100bp
-    #count=0
-    #for fragment in seq_list:
-    #    count+=1
-    #    print(name)
-    #    print(fragment)
-    #    print "+"
+        fake_quality = "J" * int(len(seq))  
+        print(fake_quality)   # This is simplistic, but fastq requires quality scores so I just add J.
